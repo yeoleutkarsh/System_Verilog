752c3d1d26ea581b64975ab9f6bf1ec2dd74b808
yeoleutkarsh <101377394+yeoleutkarsh@users.noreply.github.com>
2025-10-10 00:34:16 +0530

    Update Func_Coverage_Practice_example.sv

--- Coverage/Func_Coverage_Practice_example.sv
+++ Coverage/Func_Coverage_Practice_example.sv
@@ -1,5 +1,3 @@
-/*
-
 // Triggering of covergroup using sample method
 class pkt;
   rand bit [2:0] a;
@@ -93,30 +91,23 @@ endmodule
 // 3) using the event
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////****************************************//////////////////////////////////////////////////////////
-
-
-/*
+//////////////////////////////////////////////////****************************************/////////////////////////////////////////////////////////
 // Coverpoint
 class pkt_1;
   rand bit [3:0] a;
   rand bit [3:0] b;
   rand bit [7:0] c;
-
   function void display();
     $display("a=%0d and b=%0d", a, b);
   endfunction
-
 endclass
 
 module test_1();
   pkt_1 p;
-  
   covergroup cg;
     A1: coverpoint p.a;
     A2: coverpoint p.b;
   endgroup
-
   initial begin
     cg c = new();
     p = new();
@@ -132,11 +123,9 @@ endmodule
 // Coverpoint Part Select
 module test_2();
   pkt_1 p;
-
   covergroup cg;
     A1: coverpoint p.c[3:1];
   endgroup
-
   initial begin
     cg c = new();
     p = new();
@@ -145,28 +134,22 @@ module test_2();
     c.sample();
     $display("C=%0b", p.c);
   end
-
 endmodule
 
 // Coverpoint expressions
 class pkt_2;
   rand bit [3:0] a;
   rand bit [3:0] b;
-
   constraint c1{
     a == 5;
     b == 4;
   }
-
 endclass
-
 module test_3();
   pkt_2 p;
-
   covergroup cg;
     A1: coverpoint p.a + p.b;
   endgroup
-
   initial begin
     cg c = new();
     p = new();
@@ -178,50 +161,41 @@ endmodule
 // Coverpoint multiple expression
 module test_4();
   pkt_2 p;
-
   covergroup cg;
     A1: coverpoint { p.a - p.b , p.a + p.b };
   endgroup
-
   initial begin
-  cg c = new();
-  p = new();
-  p.randomize();
-  c.sample;
+    cg c = new();
+    p = new();
+    p.randomize();
+    c.sample;
   end
-
 endmodule
 
 // Coverpoint function call
 class pkt_3;
   rand bit [0:3] a;
   rand bit [0:3] b;
-
   function sum ( bit c );
     return c;
   endfunction
-
   constraint c1{
     a == 5;
     b == 5;
   }
-
   function bit [3:0] carry ( bit [3:0] d, e );
     if ( d == 5 && e == 5 ) carry = 5;
     if ( d == 6 && e == 6 ) carry = 6;
     return carry;
   endfunction
-
 endclass
 
 module test_5;
   pkt_3 p;
-
   covergroup cg;
     A1: coverpoint p.sum( p.a );
     A2: coverpoint p.carry( 5, 5 );
   endgroup
-
   initial begin
     cg c = new();
     p = new();
@@ -232,23 +206,18 @@ module test_5;
       c.sample();#1;
     end
   end
-
 endmodule
-*/
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////*******************************************************////////////////////////////////////////////////
 // Use of Explicit Bins
-/*
 program top;
   bit [2:0] a;
-
   covergroup cg;
     A1: coverpoint a{
         bins a_bin = { 0, 1, 2, 3, 4, 5};
     }
   endgroup
-
   cg c = new();
   initial begin
     for( int i=0; i<4; i++)
@@ -259,12 +228,10 @@ program top;
     end
   end
 endprogram
-
-// Example 2
+/////////////////////////////// Example 2
 program top_1;
   bit [0:2] y;
   bit [0:2] values[$] = '{1,7};
-
   covergroup cg;
     A1: coverpoint y{
         bins a = {0,1};
@@ -273,9 +240,7 @@ program top_1;
         bins d = {6,7};
     }
   endgroup
-
   cg c = new();
-
   initial begin
     foreach( values[i] )
     begin
@@ -285,12 +250,10 @@ program top_1;
   end
 endprogram
 
-// Example 3
+/////////////////////////////////////////////////////// Example 3
 typedef enum{ write, read, w_r} type_id;
-
 program top1;
   type_id id;
-
   covergroup cg;
     A1: coverpoint id{
         bins write = {0};
@@ -298,7 +261,6 @@ program top1;
         bins w_r   = {2};
     }
   endgroup
-
   cg c = new();
   initial begin
     id = write;
@@ -306,16 +268,15 @@ program top1;
     $display("%s::id=%0d", id, id);
   end
 endprogram
-// Example 4
+
+//////////////////////////////////////////////////// Example 4
 program top2;
   bit [2:0] a;
-
   covergroup cg;
     A1: coverpoint a{
         bins name[8] = {[0:7]};
     }
   endgroup
-
   cg c = new();
   initial begin
     for( int i=0; i<8; i++ )
@@ -325,28 +286,23 @@ program top2;
       c.sample();
     end
   end
-
 endprogram
 
-// Array Of Bins Example 1
+///////////////////////////////////////////////////////// Array Of Bins Example 1
 class pkt0;
   randc bit [7:0] a;
-
   constraint c1{
       a >= 0;
       a <= 15;
   }
 endclass
-
 module top3;
   pkt0 p;
-
   covergroup cg;
     A1: coverpoint p.a {
         bins name[8] = {[0:15]};  // name[0] = {0,1}, name[1] = {2,3},........, name[7] = {14,15} ;
     }
   endgroup
-
   initial begin
     cg c = new();
     repeat( 5 )
@@ -358,21 +314,17 @@ module top3;
     end
   end
 endmodule
-
-// Array of bins example 2
+////////////////////////////////////////////////////////// Array of bins example 2
 class pkt01;
   randc bit [2:0] a;
 endclass
-
 module top4;
   pkt01 p;
-
   covergroup cg;
     A1: coverpoint p.a {
         bins name[2] = {0,2,3,4,5};
     }
   endgroup
-
   initial begin
     cg c = new();
     repeat( 2 )
@@ -384,21 +336,17 @@ module top4;
     end
   end
 endmodule
-
-// Array of bins example 3
+//////////////////////////////////////////////////////////////// Array of bins example 3
 class pkt02;
   rand bit [3:0] a;
 endclass
-
 module top5;
   pkt02 p;
-
   covergroup cg;
     A1: coverpoint p.a {
         bins name[] = {1, 2, [3:10]};
     }
   endgroup
-
   initial begin
     cg c = new();
     repeat( 10 )
@@ -411,8 +359,6 @@ module top5;
   end
 endmodule
 
-*/
-
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////************************************************/////////////////////////////////////////////
 // How to execute coverage conditionality
@@ -420,24 +366,19 @@ endmodule
 // 1) iff constuct
 // 2) start, stop function
 
-
 // Example 1 using iff
 class pkt;
   rand bit [3:0] a;
   rand bit [3:0] b;
-
   constraint c1 { b==5; }
-
 endclass
 
 module top;
   pkt p;
-
   covergroup cg;
     // A1: coverpoint p.a iff ( p.b == 6 ); 
        A1: coverpoint p.a iff ( p.b == 5 ); 
   endgroup
-
   initial begin
     cg c = new();
     repeat( 10 )
@@ -449,7 +390,6 @@ module top;
     end
   end
 endmodule
-
 // stop and start function.
 class pkt0;
   rand bit a;
@@ -458,13 +398,11 @@ endclass
 module top1;
   bit reset;
   pkt0 p;
-
   covergroup cg;
     A1: coverpoint p.a { 
       bins name[1] = {1};
     }  
   endgroup
-
   initial begin
     cg c = new();
     p = new();
@@ -479,31 +417,4 @@ module top1;
       $display("%t :: a=%0d", $time, p.a);
     end
   end
-
 endmodule
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
